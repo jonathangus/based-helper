626abf790511a7bc40ab740f20e60d3687f5d76e
Jonathan <jonathangus@proton.me>
2025-02-09 11:56:30 +0100

    bing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import { HomePageView } from '@/components/home-page-view';
 
 export const metadata = {
-  title: 'Based Helper - Smart Token Allocation',
+  title: 'Based Helper',
+  image: '/og-image.png',
   description:
     'Based Helper is a smart token allocation tool that helps you allocate your ETH into trending tokens we think looking juicy',
 };
